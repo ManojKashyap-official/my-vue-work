25de9770bfa011f8b43c10fab27d1b57dcfad50e
manoj kashyap <monty.kashyap2@gmail.comgit config --global user.name manoj>
2020-04-27 13:03:04 +0530

    first commit

---
+++ pages/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <input type="text" v-model="userId">
+    <button @click="onloaduser">load user</button>
+</div>
+    
+    
+</template>
+<script>
+export default {
+    data(){
+        return{
+            userId:''
+        }
+},
+methods: {
+    onloaduser(){
+        this.$router.push('/users/' +this.userId)
+        }
+        }
+}
+</script>
